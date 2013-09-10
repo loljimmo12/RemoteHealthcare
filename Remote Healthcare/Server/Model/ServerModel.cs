16762c652e70a46eac4f0c2cc5a9d4deb28c7299
imp <johannes.san@gmail.com>
2013-09-10 12:39:26 +0200

    Implemented the struct Log and a method to write a Log to a bin file.

--- Remote Healthcare/Server/Model/ServerModel.cs
+++ Remote Healthcare/Server/Model/ServerModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,33 @@ namespace Server.Controller
 {
     class ServerModel
     {
+        private static String logFileName = "logData.bin";
+
+        public ServerModel()
+        {
+        }
+
+        public void saveLog(Log log)
+        {
+            using (FileStream filestream = new FileStream(logFileName, FileMode.Append, FileAccess.Write))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(filestream, log);
+            }
+        }
+    }
+
+    struct Log
+    {
+        String clientName { get; set; }
+        DateTime startSession { get; set; }
+        DateTime endSession { get; set; }
+
+        public Log(String clientName, DateTime startSession, DateTime endSession)
+        {
+            this.clientName = clientName;
+            this.startSession = startSession;
+            this.endSession = endSession;
+        }
     }
 }
