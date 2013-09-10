dad9a0e02850cf7f2710cff968546bc74488fbe8
imp <johannes.san@gmail.com>
2013-09-10 14:03:40 +0200

    Added comments to methods and classes.

--- Remote Healthcare/Server/Model/ServerModel.cs
+++ Remote Healthcare/Server/Model/ServerModel.cs
@@ -8,16 +8,32 @@ using System.Threading.Tasks;
 
 namespace Server.Controller
 {
+    // The base class for the Model.
     class ServerModel
     {
         private static String logFileName = "logData.bin";
         List<Log> logs;
 
+        // initalization of a Model object.
         public ServerModel()
         {
             logs = readLogData();
         }
 
+        // WiP
+        // Reads the specified File and returns a List of Objects from the file.
+        public List<Object> readBikeData(String clientFile)
+        {
+            BinaryFormatter serializer = new BinaryFormatter();
+            using (FileStream stream = File.OpenRead(clientFile))
+            {
+                List<Object> clientData = (List<Object>)serializer.Deserialize(stream);
+                return clientData;
+            }
+        }
+
+        // Reads the logFile and puts the data in the List.
+        // returns the created List.
         private List<Log> readLogData()
         {
             BinaryFormatter serializer = new BinaryFormatter();
@@ -28,6 +44,8 @@ namespace Server.Controller
             }
         }
 
+        // The method to add a single Log object to the List.
+        // The old logFile will be overwritten with the new List.
         public void saveLog(Log log)
         {
             logs.Add(log);
@@ -39,6 +57,8 @@ namespace Server.Controller
         }
     }
 
+    // The base class for a Log, 
+    // containing the client's name and the start- and endtime of the session.
     [Serializable]
     class Log 
     {
@@ -46,6 +66,7 @@ namespace Server.Controller
         DateTime startSession { get; set; }
         DateTime endSession { get; set; }
 
+        // initalization of a Log object.
         public Log(String name, DateTime start, DateTime end)
         {
             clientName = name;
