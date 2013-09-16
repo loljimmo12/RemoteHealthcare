b9f3ed341cec2950e0839a85c66c1936ccc9eab0
loljimmo12 <spongebob.958@gmail.com>
2013-09-16 11:55:10 +0000

    Made the base for the system

---
+++ Remote Healthcare/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        Connection connect;
+        public Form2()
+        {
+            InitializeComponent();
+            connect = new Connection();
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                connect.Login(textBox1.Text, textBox2.Text);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Remote Healthcare/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Kettler_X7_Lib.Classes;
+using Kettler_X7_Lib.Objects;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form2());
+        }
+    }
+
+    class Connection
+    {
+        public void Login(string login, string password)
+        {
+            Kettler_X7_Lib.Objects.Packet Pack = new Packet();
+            Pack.Flag = Packet.PacketFlag.PACKETFLAG_REQUEST_HANDSHAKE;
+            Pack.Data = new Kettler_X7_Lib.Objects.Handshake()
+            {
+                ClientFlag = Client.ClientFlag.CLIENTFLAG_DOCTORAPP,
+                Username = login,
+                Password = password
+
+            };
+        }
+ 
+    }
+}
